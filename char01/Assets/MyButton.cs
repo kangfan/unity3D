528fc00415ebc6b1da7bd3f132a68a48ffa609d8
confiner <confiner.it@gmail.com>
2015-02-12 17:35:47 +0800

    something about the gui of rpg game

---
+++ char01/Assets/MyButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * @describe 创建按钮
+ * @author 康自军
+ * @time 
+ */
+public class MyButton : MonoBehaviour {
+	public Texture2D bg;
+	public float left;
+	public float top;
+	void OnGUI()
+	{
+		if ( bg == null) 
+		{
+			Debug.LogError("Please assign a texture on the inspector");
+			print ( "enter the params" );
+			return;
+		}
+
+		GUI.Button ( new Rect( left, top, bg.width, bg.height ), bg );
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
